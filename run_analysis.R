253ee407e82cd4eb706e1572f24aeedbc9a385a3
zgb3l <zgb3l@outlook.com>
2021-12-19 15:46:55 +0100

    Naming columns

---
+++ run_analysis.R
@@ -0,0 +1,23 @@
+# Getting the data:
+
+if(!file.exists("./data")){dir.create("./data")}
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileUrl, destfile = "./data/Dataset.zip")
+unzip(zipfile = "./data/Dataset.zip", exdir = "./data")
+
+# Creating R script called run_analysis.R that does the following: 
+
+# (1) Merges the training and the test sets to create one data set.
+
+# 1.1 Reading files + training tables:
+
+x_train <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+y_train <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+subject_train <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
+
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names. 
+# 5. From the data set in step 4, creates a second, independent tidy data set 
+# with the average of each variable for each activity and each subject.
+
